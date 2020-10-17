d73e07871ccc43a907b54a02874f5e5adaf628e6
Luis Peralta <luis_peralta_000@hotmail.com>
2020-10-17 14:44:55 -0300

    actualizacion 2.0

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 document.getElementById('color').addEventListener('input', getColor)
 
-function getColor(){
+function getColor() {
     let col = document.getElementById('color').value
 
-    document.getElementById('visualizar').style.background=col
-    document.getElementById('visualizar').innerHTML=col
+    document.getElementById('visualizar').style.background = col
+    document.getElementById('visualizar').innerHTML = col
 }
+
